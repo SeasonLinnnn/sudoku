fdf313cb67c8cccdbcb57e7e8ea36413793d6d2c
SeasonLinnnn <68676985+SeasonLinnnn@users.noreply.github.com>
2021-04-16 10:43:25 +0800

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -87,3 +87,63 @@ void sudoku::gen_sudoku_result(int num)
 		} while (next_permutation(swap, swap + 3));
 	} while (next_permutation(first_row, first_row + 9));
 }
+bool sudoku::is_unique(int current_sudoku_index, int row, int column, int num, int sudo[][9])
+{
+	int temp_sudoku[9][9][9];//用于判断生成的数独结果是否唯一,至少要测试其他八个数
+	sudo[row + current_sudoku_index * 9][column] = 0;
+	//将这些要测试的数需要用到的临时数独都初始化
+	for (int m = 0; m < 9; m++)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				temp_sudoku[m][i][j] = sudo[current_sudoku_index * 9 + i][j];
+			}
+		}
+	}
+	int answerCount = 0;
+	for (int i = 1; i < 10; i++)
+	{
+		if (is_right(0, row, column, i, temp_sudoku[i - 1]))
+		{
+			temp_sudoku[i - 1][row][column] = i;
+			if (dfs(0, row, column, temp_sudoku[i - 1]))
+				answerCount++;
+		}
+	}
+	if (answerCount != 1)
+	{
+		sudo[row + current_sudoku_index * 9][column] = num;
+		return false;
+	}
+	return true;
+}
+bool sudoku::is_right(int current_sudoku_index, int row, int column, int num, int sudo[][9])
+{
+	/*数独需要一行里出现1-9九个数字没有重复，同理行和
+	九个九宫格都是如此*/
+
+	//列判断
+	for (int i = 0; i < 9; i++)
+	{
+		if (sudo[current_sudoku_index * 9 + i][column] == num)
+			return false;
+	}
+	//行判断
+	for (int i = 0; i < 9; i++)
+	{
+		if (sudo[current_sudoku_index * 9 + row][i] == num)
+			return false;
+	}
+	//九宫格判断
+	for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++)
+	{
+		for (int j = column / 3 * 3; j < column / 3 * 3 + 3; j++)
+		{
+			if (sudo[current_sudoku_index * 9 + i][j] == num)
+				return false;
+		}
+	}
+	return true;
+}
